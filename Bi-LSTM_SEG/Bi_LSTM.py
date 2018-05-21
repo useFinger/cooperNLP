dae1a094a4fda7f9eeb913ac928b4a1a565adcd7
useFinger <finger@MacBook-Pro.local>
2018-05-21 12:40:59 +0800

    lstm

---
+++ Bi-LSTM_SEG/Bi_LSTM.py
@@ -0,0 +1,341 @@
+import tensorflow as tf
+config = tf.ConfigProto()
+config.gpu_options.allow_growth = True
+sess = tf.Session(config=config)
+from tensorflow.contrib import rnn
+import numpy as np
+import data_generator as da
+import time
+
+data_train, data_valid, data_test, word2id, id2word, tag2id, id2tag, labels = da.data_gen()
+'''
+For Chinese word segmentation.
+'''
+# ##################### config ######################
+decay = 0.85
+max_epoch = 5
+max_max_epoch = 10
+timestep_size = max_len = 32           # 句子长度
+vocab_size = 5159    # 样本中不同字的个数+1(padding 0)，根据处理数据的时候得到
+input_size = embedding_size = 64       # 字向量长度
+class_num = 5
+hidden_size = 128    # 隐含层节点数
+layer_num = 2        # bi-lstm 层数
+max_grad_norm = 5.0  # 最大梯度（超过此值的梯度将被裁剪）
+
+lr = tf.placeholder(tf.float32, [])
+keep_prob = tf.placeholder(tf.float32, [])
+batch_size = tf.placeholder(tf.int32, [])  # 注意类型必须为 tf.int32
+model_save_path = 'ckpt/bi-lstm.ckpt'  # 模型保存位置
+
+
+with tf.variable_scope('embedding'):
+    embedding = tf.get_variable("embedding", [vocab_size, embedding_size], dtype=tf.float32)
+
+def weight_variable(shape):
+    """Create a weight variable with appropriate initialization."""
+    initial = tf.truncated_normal(shape, stddev=0.1)
+    return tf.Variable(initial)
+
+def bias_variable(shape):
+    """Create a bias variable with appropriate initialization."""
+    initial = tf.constant(0.1, shape=shape)
+    return tf.Variable(initial)
+
+def lstm_cell():
+    cell = rnn.LSTMCell(hidden_size, reuse=tf.get_variable_scope().reuse)
+    return rnn.DropoutWrapper(cell, output_keep_prob=keep_prob)
+         
+def bi_lstm(X_inputs):
+    """build the bi-LSTMs network. Return the y_pred"""
+    # X_inputs.shape = [batchsize, timestep_size]  ->  inputs.shape = [batchsize, timestep_size, embedding_size]
+    inputs = tf.nn.embedding_lookup(embedding, X_inputs)  
+    
+    # ** 1.构建前向后向多层 LSTM
+    cell_fw = rnn.MultiRNNCell([lstm_cell() for _ in range(layer_num)], state_is_tuple=True)
+    cell_bw = rnn.MultiRNNCell([lstm_cell() for _ in range(layer_num)], state_is_tuple=True)
+  
+    # ** 2.初始状态
+    initial_state_fw = cell_fw.zero_state(batch_size, tf.float32)
+    initial_state_bw = cell_bw.zero_state(batch_size, tf.float32)  
+    
+    # 下面两部分是等价的
+    # **************************************************************
+    # ** 把 inputs 处理成 rnn.static_bidirectional_rnn 的要求形式
+    # ** 文档说明
+    # inputs: A length T list of inputs, each a tensor of shape
+    # [batch_size, input_size], or a nested tuple of such elements.
+    # *************************************************************
+    # Unstack to get a list of 'n_steps' tensors of shape (batch_size, n_input)
+    # inputs.shape = [batchsize, timestep_size, embedding_size]  ->  timestep_size tensor, each_tensor.shape = [batchsize, embedding_size]
+    # inputs = tf.unstack(inputs, timestep_size, 1)
+    # ** 3.bi-lstm 计算（tf封装）  一般采用下面 static_bidirectional_rnn 函数调用。
+    #   但是为了理解计算的细节，所以把后面的这段代码进行展开自己实现了一遍。
+#     try:
+#         outputs, _, _ = rnn.static_bidirectional_rnn(cell_fw, cell_bw, inputs, 
+#                         initial_state_fw = initial_state_fw, initial_state_bw = initial_state_bw, dtype=tf.float32)
+#     except Exception: # Old TensorFlow version only returns outputs not states
+#         outputs = rnn.static_bidirectional_rnn(cell_fw, cell_bw, inputs, 
+#                         initial_state_fw = initial_state_fw, initial_state_bw = initial_state_bw, dtype=tf.float32)
+#     output = tf.reshape(tf.concat(outputs, 1), [-1, hidden_size * 2])
+    # ***********************************************************
+    
+    # ***********************************************************
+    # ** 3. bi-lstm 计算（展开）
+    with tf.variable_scope('bidirectional_rnn'):
+        # *** 下面，两个网络是分别计算 output 和 state 
+        # Forward direction
+        outputs_fw = list()
+        state_fw = initial_state_fw
+        with tf.variable_scope('fw'):
+            for timestep in range(timestep_size):
+                if timestep > 0:
+                    tf.get_variable_scope().reuse_variables()
+                (output_fw, state_fw) = cell_fw(inputs[:, timestep, :], state_fw)
+                outputs_fw.append(output_fw)
+        
+        # backward direction
+        outputs_bw = list()
+        state_bw = initial_state_bw
+        with tf.variable_scope('bw') as bw_scope:
+            inputs = tf.reverse(inputs, [1])
+            for timestep in range(timestep_size):
+                if timestep > 0:
+                    tf.get_variable_scope().reuse_variables()
+                (output_bw, state_bw) = cell_bw(inputs[:, timestep, :], state_bw)
+                outputs_bw.append(output_bw)
+        # *** 然后把 output_bw 在 timestep 维度进行翻转
+        # outputs_bw.shape = [timestep_size, batch_size, hidden_size]
+        outputs_bw = tf.reverse(outputs_bw, [0])
+        # 把两个oupputs 拼成 [timestep_size, batch_size, hidden_size*2]
+        output = tf.concat([outputs_fw, outputs_bw], 2)
+        output = tf.transpose(output, perm=[1,0,2])
+        output = tf.reshape(output, [-1, hidden_size*2])
+    # ***********************************************************
+    return output # [-1, hidden_size*2]
+
+
+with tf.variable_scope('Inputs'):
+    X_inputs = tf.placeholder(tf.int32, [None, timestep_size], name='X_input')
+    y_inputs = tf.placeholder(tf.int32, [None, timestep_size], name='y_input')   
+    
+bilstm_output = bi_lstm(X_inputs)
+
+with tf.variable_scope('outputs'):
+    softmax_w = weight_variable([hidden_size * 2, class_num]) 
+    softmax_b = bias_variable([class_num]) 
+    y_pred = tf.matmul(bilstm_output, softmax_w) + softmax_b
+
+# adding extra statistics to monitor
+# y_inputs.shape = [batch_size, timestep_size]
+correct_prediction = tf.equal(tf.cast(tf.argmax(y_pred, 1), tf.int32), tf.reshape(y_inputs, [-1]))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+cost = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(labels = tf.reshape(y_inputs, [-1]), logits = y_pred))
+
+# ***** 优化求解 *******
+tvars = tf.trainable_variables()  # 获取模型的所有参数
+grads, _ = tf.clip_by_global_norm(tf.gradients(cost, tvars), max_grad_norm)  # 获取损失函数对于每个参数的梯度
+optimizer = tf.train.AdamOptimizer(learning_rate=lr)   # 优化器
+
+# 梯度下降计算
+# train_op = optimizer.apply_gradients( zip(grads, tvars),
+#     global_step=tf.contrib.framework.get_or_create_global_step())
+train_op = optimizer.apply_gradients( zip(grads, tvars),
+    global_step=tf.train.get_or_create_global_step())
+print ('Finished creating the bi-lstm model.')
+
+
+
+
+def test_epoch(dataset):
+    """Testing or valid."""
+    _batch_size = 500
+    fetches = [accuracy, cost]
+    _y = dataset.y
+    data_size = _y.shape[0]
+    batch_num = int(data_size / _batch_size)
+    start_time = time.time()
+    _costs = 0.0
+    _accs = 0.0
+    for i in range(batch_num):
+        X_batch, y_batch = dataset.next_batch(_batch_size)
+        feed_dict = {X_inputs:X_batch, y_inputs:y_batch, lr:1e-5, batch_size:_batch_size, keep_prob:1.0}
+        _acc, _cost = sess.run(fetches, feed_dict)
+        _accs += _acc
+        _costs += _cost    
+    mean_acc= _accs / batch_num     
+    mean_cost = _costs / batch_num
+    return mean_acc, mean_cost
+
+
+sess.run(tf.global_variables_initializer())
+tr_batch_size = 128 
+max_max_epoch = 6
+display_num = 5  # 每个 epoch 显示是个结果
+tr_batch_num = int(data_train.y.shape[0] / tr_batch_size)  # 每个 epoch 中包含的 batch 数
+display_batch = int(tr_batch_num / display_num)  # 每训练 display_batch 之后输出一次
+saver = tf.train.Saver(max_to_keep=10)  # 最多保存的模型数量
+
+for epoch in range(max_max_epoch):
+    _lr = 1e-4
+    if epoch > max_epoch:
+        _lr = _lr * ((decay) ** (epoch - max_epoch))
+    print ('EPOCH %d， lr=%g' % (epoch+1, _lr))
+    start_time = time.time()
+    _costs = 0.0
+    _accs = 0.0
+    show_accs = 0.0
+    show_costs = 0.0
+    for batch in range(tr_batch_num): 
+        fetches = [accuracy, cost, train_op]
+        X_batch, y_batch = data_train.next_batch(tr_batch_size)
+        feed_dict = {X_inputs:X_batch, y_inputs:y_batch, lr:_lr, batch_size:tr_batch_size, keep_prob:0.5}
+        _acc, _cost, _ = sess.run(fetches, feed_dict) # the cost is the mean cost of one batch
+        _accs += _acc
+        _costs += _cost
+        show_accs += _acc
+        show_costs += _cost
+        if (batch + 1) % display_batch == 0:
+            valid_acc, valid_cost = test_epoch(data_valid)  # valid
+            print ('\ttraining acc=%g, cost=%g;  valid acc= %g, cost=%g ' % (show_accs / display_batch,
+                                                show_costs / display_batch, valid_acc, valid_cost))
+            show_accs = 0.0
+            show_costs = 0.0
+    mean_acc = _accs / tr_batch_num 
+    mean_cost = _costs / tr_batch_num
+    if (epoch + 1) % 3 == 0:  # 每 3 个 epoch 保存一次模型
+        save_path = saver.save(sess, model_save_path, global_step=(epoch+1))
+        print ('the save path is ', save_path)
+    print ('\ttraining %d, acc=%g, cost=%g ' % (data_train.y.shape[0], mean_acc, mean_cost))
+    print ('Epoch training %d, acc=%g, cost=%g, speed=%g s/epoch' % (data_train.y.shape[0], mean_acc, mean_cost, time.time()-start_time))        
+# testing
+print ('**TEST RESULT:')
+test_acc, test_cost = test_epoch(data_test)
+print ('**Test %d, acc=%g, cost=%g' % (data_test.y.shape[0], test_acc, test_cost))
+
+# ** 导入模型
+saver = tf.train.Saver()
+best_model_path = 'ckpt/bi-lstm.ckpt-6'
+saver.restore(sess, best_model_path)
+X_tt, y_tt = data_train.next_batch(2)
+print ('X_tt.shape=', X_tt.shape, 'y_tt.shape=', y_tt.shape)
+print ('X_tt = ', X_tt)
+print ('y_tt = ', y_tt)
+
+
+# 利用 labels（即状态序列）来统计转移概率
+# 因为状态数比较少，这里用 dict={'I_tI_{t+1}'：p} 来实现
+# A统计状态转移的频数
+A = {
+      'sb':0,
+      'ss':0,
+      'be':0,
+      'bm':0,
+      'me':0,
+      'mm':0,
+      'eb':0,
+      'es':0
+     }
+
+# zy 表示转移概率矩阵
+zy = dict()
+for label in labels:
+    for t in range(len(label) - 1):
+        key = label[t] + label[t+1]
+        A[key] += 1.0
+        
+zy['sb'] = A['sb'] / (A['sb'] + A['ss'])
+zy['ss'] = 1.0 - zy['sb']
+zy['be'] = A['be'] / (A['be'] + A['bm'])
+zy['bm'] = 1.0 - zy['be']
+zy['me'] = A['me'] / (A['me'] + A['mm'])
+zy['mm'] = 1.0 - zy['me']
+zy['eb'] = A['eb'] / (A['eb'] + A['es'])
+zy['es'] = 1.0 - zy['eb']
+keys = sorted(zy.keys())
+print ('the transition probability: ')
+for key in keys:
+    print (key, zy[key])
+    
+zy = {i:np.log(zy[i]) for i in zy.keys()}
+
+
+def viterbi(nodes):
+    """
+    维特比译码：除了第一层以外，每一层有4个节点。
+    计算当前层（第一层不需要计算）四个节点的最短路径：
+       对于本层的每一个节点，计算出路径来自上一层的各个节点的新的路径长度（概率）。保留最大值（最短路径）。
+       上一层每个节点的路径保存在 paths 中。计算本层的时候，先用paths_ 暂存，然后把本层的最大路径保存到 paths 中。
+       paths 采用字典的形式保存（路径：路径长度）。
+       一直计算到最后一层，得到四条路径，将长度最短（概率值最大的路径返回）
+    """
+    paths = {'b': nodes[0]['b'], 's':nodes[0]['s']} # 第一层，只有两个节点
+    for layer in range(1, len(nodes)):  # 后面的每一层
+        paths_ = paths.copy()  # 先保存上一层的路径
+        # node_now 为本层节点， node_last 为上层节点
+        paths = {}  # 清空 path 
+        for node_now in nodes[layer].keys():
+            # 对于本层的每个节点，找出最短路径
+            sub_paths = {} 
+            # 上一层的每个节点到本层节点的连接
+            for path_last in paths_.keys():
+                if path_last[-1] + node_now in zy.keys(): # 若转移概率不为 0 
+                    sub_paths[path_last + node_now] = paths_[path_last] + nodes[layer][node_now] + zy[path_last[-1] + node_now]
+            # 最短路径,即概率最大的那个
+            sr_subpaths = pd.Series(sub_paths)
+            sr_subpaths = sr_subpaths.sort_values()  # 升序排序
+            node_subpath = sr_subpaths.index[-1]  # 最短路径
+            node_value = sr_subpaths[-1]   # 最短路径对应的值
+            # 把 node_now 的最短路径添加到 paths 中
+            paths[node_subpath] = node_value
+    # 所有层求完后，找出最后一层中各个节点的路径最短的路径
+    sr_paths = pd.Series(paths)
+    sr_paths = sr_paths.sort_values()  # 按照升序排序
+    return sr_paths.index[-1]  # 返回最短路径（概率值最大的路径）
+
+
+def text2ids(text):
+    """把字片段text转为 ids."""
+    words = list(text)
+    ids = list(word2id[words])
+    if len(ids) >= max_len:  # 长则弃掉
+        print (u'输出片段超过%d部分无法处理' % (max_len) )
+        return ids[:max_len]
+    ids.extend([0]*(max_len-len(ids))) # 短则补全
+    ids = np.asarray(ids).reshape([-1, max_len])
+    return ids
+
+
+def simple_cut(text):
+    """对一个片段text（标点符号把句子划分为多个片段）进行预测。"""
+    if text:
+        text_len = len(text)
+        X_batch = text2ids(text)  # 这里每个 batch 是一个样本
+        fetches = [y_pred]
+        feed_dict = {X_inputs:X_batch, lr:1.0, batch_size:1, keep_prob:1.0}
+        _y_pred = sess.run(fetches, feed_dict)[0][:text_len]  # padding填充的部分直接丢弃
+        nodes = [dict(zip(['s','b','m','e'], each[1:])) for each in _y_pred]
+        tags = viterbi(nodes)
+        words = []
+        for i in range(len(text)):
+            if tags[i] in ['s', 'b']:
+                words.append(text[i])
+            else:
+                words[-1] += text[i]
+        return words
+    else:
+        return []
+
+
+def cut_word(sentence):
+    """首先将一个sentence根据标点和英文符号/字符串划分成多个片段text，然后对每一个片段分词。"""
+    not_cuts = re.compile(u'([0-9\da-zA-Z ]+)|[。，、？！.\.\?,!]')
+    result = []
+    start = 0
+    for seg_sign in not_cuts.finditer(sentence):
+        result.extend(simple_cut(sentence[start:seg_sign.start()]))
+        result.append(sentence[seg_sign.start():seg_sign.end()])
+        start = seg_sign.end()
+    result.extend(simple_cut(sentence[start:]))
+    return result
+
